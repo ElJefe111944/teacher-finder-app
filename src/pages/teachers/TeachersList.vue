e5a5925f9436d47490df0c6da3b86234e22d29c8
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-02-26 11:18:03 +0000

    filteredTeachers getter added to TeacherList component and list of teachers now rendered on page

--- src/pages/teachers/TeachersList.vue
+++ src/pages/teachers/TeachersList.vue
@@ -10,10 +10,25 @@
             <router-link to="/register">Register as a Teacher</router-link>
         </div>
         <!-- list of teachers -->
-        List of teachers
+        <ul>
+            <li v-for="teacher in filteredTeachers" :key="teacher.id">
+            {{ teacher.firstName }}
+            </li>
+        </ul>
     </section>
     </template>
     
     <script>
+
+    export default {
+
+        computed: {
+            filteredTeachers(){
+                // pull data from teacherModule in store
+                // getters [namespaced name / getter name]
+                return this.$store.getters['teachers/teachers'];
+            }
+        }
+    }
     
     </script>
--- src/store/modules/teachers/getters.js
+++ src/store/modules/teachers/getters.js
@@ -1,3 +1,9 @@
 export default {
-    
+    teachers(state){
+        return state.teachers;
+    },
+    hasTeachers(state){
+        // check to verify that teachers array isnt empty
+        return state.teachers && state.teachers.length > 0;
+    }    
 };
