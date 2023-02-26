63fe67b080dc2e6e05c9acc9163376a6f15d4d7f
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-02-26 09:47:05 +0000

    All routes wired up and template tags added

--- src/router.js
+++ src/router.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import TeacherDetail from './pages/teachers/TeacherDetail.vue';
+import TeachersList from './pages/teachers/TeachersList.vue';
+import TeacherRegistration from './pages/teachers/TeacherRegistration.vue';
+import ContactTeacher from './pages/requests/ContactTeacher.vue';
+import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
+import NotFound from './pages/NotFound.vue'
+
 // router configure
 const router = createRouter({
     history: createWebHistory(),
@@ -8,18 +15,18 @@ const router = createRouter({
         // make all teachers the landing page
         { path: '/', redirect: '/teachers' },
         // all teachers
-        { path: '/teachers', component: null },
+        { path: '/teachers', component: TeachersList },
         // individual teacher details page
-        { path: '/teacher/:id', component: null, children: [
+        { path: '/teacher/:id', component: TeacherDetail, children: [
             // nested route to contact teacher
-            { path: 'contact', component: null }, // /teachers/t1/contact
+            { path: 'contact', component: ContactTeacher }, // /teachers/t1/contact
         ] },
         // register as teacher
-        { path: '/register', component: null },
+        { path: '/register', component: TeacherRegistration },
         // view all requests
-        { path: '/requests', component: null },
+        { path: '/requests', component: RequestsRecieved },
         // 404 / page not found
-        { path: '/:notFound(.*)', component: null },
+        { path: '/:notFound(.*)', component: NotFound },
     ]
 });
 
