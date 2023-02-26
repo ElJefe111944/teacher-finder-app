b824b6cfbb3d4a6117e0d9312520447ff6021584
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-02-26 11:03:56 +0000

    teacherModule created in store and dummy data added

---
+++ src/store/modules/teachers/index.js
@@ -0,0 +1,30 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+    namespaced: true,
+    state(){
+        teachers: [
+            {
+                id: 't1',
+                firstName: 'Pablo',
+                lastName: 'Gonzalez',
+                arears: ['DELE preparation', 'Adults'],
+                description: 'Pablo has been a teacvher for over 15 years',
+                hourlyRate: 25,
+            },
+            {
+                id: 't2',
+                firstName: 'Clara',
+                lastName: 'Fernandez',
+                arears: ['DELE preparation', 'Adults', 'children', 'GCSE & A-levels'],
+                description: 'Clara has been a teacvher for over 10 years',
+                hourlyRate: 30,
+            }
+        ]
+    },
+    mutations,
+    actions,
+    getters,
+}
