7de8ade21a6f27dbf86a65cc6b46c5c72f3019cf
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-02-26 12:01:57 +0000

    Teacher Item component & styling added to teacher list page

--- src/pages/teachers/TeachersList.vue
+++ src/pages/teachers/TeachersList.vue
@@ -10,25 +10,52 @@
             <router-link to="/register">Register as a Teacher</router-link>
         </div>
         <!-- list of teachers -->
-        <ul>
-            <li v-for="teacher in filteredTeachers" :key="teacher.id">
-            {{ teacher.firstName }}
-            </li>
+        <ul v-if="hasTeachers">
+            <TeacherItem v-for="teacher in filteredTeachers" 
+            :key="teacher.id" 
+            :id="teacher.id"
+            :first-name="teacher.firstName" 
+            :last-name="teacher.lastName" 
+            :rate="teacher.hourlyRate" 
+            :areas="teacher.areas" />
         </ul>
+        <!-- If no teachers are found -->
+        <h3 v-else>No teachers found...</h3>
     </section>
-    </template>
+</template>
     
     <script>
+     import TeacherItem from '../../components/teachers/TeacherItem.vue'
 
     export default {
-
+          
+        components: {
+            TeacherItem,
+        },
         computed: {
             filteredTeachers(){
                 // pull data from teacherModule in store
                 // getters [namespaced name / getter name]
                 return this.$store.getters['teachers/teachers'];
+            },
+            hasTeachers(){
+                // check to see if teachers array is empty
+                return this.$store.getters['teachers/hasTeachers'];
             }
         }
     }
     
     </script>
+
+    <style scoped>
+    ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+    </style>
