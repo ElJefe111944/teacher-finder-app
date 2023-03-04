d3cc0ee1958f859b69ea08feeb9e12a7ff4fefcb
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-04 11:10:55 +0000

    TeacherForm component created and added to TeacherRegistration component

--- src/components/teachers/TeacherForm.vue
+++ src/components/teachers/TeacherForm.vue
@@ -1,16 +1,16 @@
 <template>
-    <form>
+    <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="firstName">Firstname</label>
-            <input type="text" id="firstName" v-model="firstName">
+            <input type="text" id="firstName" v-model.trim="firstName">
         </div>
         <div class="form-control">
             <label for="lastName">Surname</label>
-            <input type="text" id="lastName" v-model="lastName">
+            <input type="text" id="lastName" v-model.trim="lastName">
         </div>
         <div class="form-control">
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5" v-model="description"></textarea>
+            <textarea name="description" id="description" rows="5" v-model.trim="description"></textarea>
         </div>
         <div class="form-control">
             <label for="rate">Hourly Rate</label>
@@ -20,19 +20,19 @@
             <h3>Areas of Expertise</h3>
             <!-- check box section -->
             <div>
-                <input type="checkbox" name="adults" id="adults">
+                <input type="checkbox" name="adults" id="adults" v-model="areas">
                 <label for="adults">Adults</label>
             </div>
             <div>
-                <input type="checkbox" name="children" id="children">
+                <input type="checkbox" name="children" id="children" v-model="areas">
                 <label for="children">Children</label>
             </div>
             <div>
-                <input type="checkbox" name="dele" id="dele">
+                <input type="checkbox" name="dele" id="dele" v-model="areas">
                 <label for="dele">DELE</label>
             </div>
             <div>
-                <input type="checkbox" name="gcse" id="gcse">
+                <input type="checkbox" name="gcse" id="gcse" v-model="areas">
                 <label for="gcse">GCSE</label>
             </div>
         </div>
@@ -51,6 +51,18 @@ export default {
             description: '',
             rate: null,
             areas: [],
+        }
+    },
+    methods: {
+        submitForm(){
+            const formData = {
+                first: this.firstName,
+                last: this.lastName,
+                desc: this.description,
+                rate: this.rate,
+                areas: this.areas,
+            };
+            console.log(formData);
         }
     }
 };
--- src/pages/teachers/TeacherRegistration.vue
+++ src/pages/teachers/TeacherRegistration.vue
@@ -1,7 +1,18 @@
 <template>
-    Registration
+    <section>
+        <h2>Register as a teacher now!</h2>
+        <TeacherForm />
+    </section>
 </template>
 
 <script>
+import TeacherForm from '@/components/teachers/TeacherForm.vue';
+
+export default {
+    components: {
+        TeacherForm,
+    }
+
+}
 
 </script>
