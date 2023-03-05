de70265c5bee35955afabe7f102962a4b90623c8
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-05 13:41:27 +0000

    Connect register as teacher to firebase - Send PUT htttp request to store teacher data functionalty added to action in teacher module of store

--- src/store/modules/teachers/actions.js
+++ src/store/modules/teachers/actions.js
@@ -1,8 +1,8 @@
 export default {
-    registerTeacher(context, payload){
+    async registerTeacher(context, payload){
+        const userId = context.rootGetters.userId;
         const teacherData = {
             // match names registration form to state
-            id: context.rootGetters.userId,
             firstName: payload.first,
             lastName: payload.last,
             description: payload.desc,
@@ -10,6 +10,22 @@ export default {
             areas: payload.areas,
         };
 
-        context.commit('registerTeacher', teacherData);
+        // send request to firebase 
+        const response = await fetch(`https://teacher-finder-app-5fba7-default-rtdb.europe-west1.firebasedatabase.app/teachers/${userId}.json`, {
+            method: 'PUT', // PUT - Overwritten if exited NOT POST
+            body: JSON.stringify(teacherData), 
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok){
+            // error handling
+
+        }
+
+        context.commit('registerTeacher', {
+            ...teacherData,
+            id: userId
+        });
     }
 };
