8f27fcf260cd734855f949ec9458da4018395d54
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-10 13:50:33 +0000

    Auth module added to app store

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,15 @@
 import { createStore } from "vuex";
 
 import teachersModule from "./modules/teachers/index.js";
-import requestsModule from './modules/requests/index.js'
+import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
     modules: {
         teachers: teachersModule,
         requests: requestsModule,
+        auth: authModule,
     },
-    state(){
-        return {
-            userId: 'c3',
-        };
-    },
-    getters: {
-        userId(state){
-            return state.userId;
-        }
-    }
 });
 
 export default store;
