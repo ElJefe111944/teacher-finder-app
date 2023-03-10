988e295ef40820b6305cc0f07356abb001f60766
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-10 12:33:38 +0000

    NotFound component styling added

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be found - maybe check out all our <router-link to="/teachers">teachers</router-link>.</p>
+        </base-card>
+    </section>
 </template>
 
 <script>
